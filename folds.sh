ea5ba73013d86272bec65407f3939988cd14d458
Thomas Wood <thomas.wood09@imperial.ac.uk>
2016-10-21 16:20:03 +0200

    wrong action names

--- folds.sh
+++ folds.sh
@@ -7,27 +7,27 @@ travis_fold() {
   echo -en "travis_fold:${action}:${name}\r${ANSI_CLEAR}"
 }
 
-travis_fold open test1
+travis_fold start test1
 echo "some output"
-travis_fold open test2
+travis_fold start test2
 echo "some output"
-travis_fold close test2
+travis_fold end test2
 echo "some output"
-travis_fold close test1
+travis_fold end test1
 
 echo "some output"
 
-travis_fold open test3
-travis_fold open test4
+travis_fold start test3
+travis_fold start test4
 echo "some output"
-travis_fold close test4
-travis_fold close test3
+travis_fold end test4
+travis_fold end test3
 
 echo "some output"
 
-travis_fold open test5
+travis_fold start test5
 echo "some output"
-travis_fold open test6
-travis_fold close test6
+travis_fold start test6
+travis_fold end test6
 echo "some output"
-travis_fold close test5
+travis_fold end test5
