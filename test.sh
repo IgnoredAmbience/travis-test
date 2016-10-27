e0a8b44f848855660550696a1cd41e9c937628ca
Thomas Wood <thomas.wood09@imperial.ac.uk>
2016-10-27 20:04:29 +0200

    test

--- test.sh
+++ test.sh
@@ -25,6 +25,7 @@ SHA=`git rev-parse --verify HEAD`
 eval `ssh-agent`
 echo -e "$DEPLOY_KEY" | ssh-add /dev/stdin
 cat ~/.ssh/config
+cat /etc/ssh/config
 ssh github.com || exit 0
 
 # Clone the existing gh-pages for this repo into out/
